56fa8fcecfb11f79aec12385ec53e96d58c49bb1
Burcu Dogan <jbd@google.com>
2015-08-18 19:37:03 -0700

    Return an events channel rather than implementing an io.Reader like interface.

--- sensor.go
+++ sensor.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package sensor provides sensor events from various movement sensors.
-package sensor // import "golang.org/x/mobile/exp/sensor"
+package sensor
 
 import (
 	"errors"
@@ -63,56 +63,28 @@ type Event struct {
 	Data []float64
 }
 
-// Manager multiplexes sensor event data from various sensor sources.
-type Manager struct {
-	m *manager // platform-specific implementation of the underlying manager
-}
+var m *manager
 
-// Enable enables a sensor with the specified delay rate.
-// If there are multiple sensors of type t on the device, Enable uses
-// the default one.
-// If there is no default sensor of type t on the device, an error returned.
-// Valid sensor types supported by this package are Accelerometer,
-// Gyroscope, Magnetometer and Altimeter.
-func (m *Manager) Enable(t Type, delay time.Duration) error {
-	if m.m == nil {
-		m.m = new(manager)
-		m.m.initialize()
-	}
+func Enable(t Type, delay time.Duration) error {
 	if t < 0 || int(t) >= len(sensorNames) {
 		return errors.New("sensor: unknown sensor type")
 	}
-	return m.m.enable(t, delay)
+	return m.enable(t, delay)
 }
 
 // Disable disables to feed the manager with the specified sensor.
-func (m *Manager) Disable(t Type) error {
-	if m.m == nil {
-		m.m = new(manager)
-		m.m.initialize()
-	}
+func Disable(t Type) error {
 	if t < 0 || int(t) >= len(sensorNames) {
 		return errors.New("sensor: unknown sensor type")
 	}
-	return m.m.disable(t)
+	return m.disable(t)
 }
 
-// Read reads a series of events from the manager.
-// It may read up to len(e) number of events, but will return
-// less events if timeout occurs.
-func (m *Manager) Read(e []Event) (n int, err error) {
-	if m.m == nil {
-		m.m = new(manager)
-		m.m.initialize()
-	}
-	return m.m.read(e)
+func Events() <-chan interface{} {
+	panic("not yet implemented")
 }
 
-// Close stops the manager and frees the related resources.
-// Once Close is called, Manager becomes invalid to use.
-func (m *Manager) Close() error {
-	if m.m == nil {
-		return nil
-	}
-	return m.m.close()
+func init() {
+	m = new(manager)
+	m.initialize()
 }
